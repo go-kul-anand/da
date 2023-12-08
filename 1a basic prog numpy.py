cd4789046388bd45cd969129495ed9e0b654c50e
go-kul-anand <152949469+go-kul-anand@users.noreply.github.com>
2023-12-08 11:59:17 +0530

    Create 1a basic prog numpy.py

---
+++ 1a basic prog numpy.py
@@ -0,0 +1,11 @@
+import numpy as np 
+list1 = [1, 2, 3] 
+list2=[4,5,6] 
+a=np.add(list1,list2) 
+b=np.subtract(list1,list2) 
+c=np.multiply(list1,list2) 
+d=np.divide(list1,list2) 
+print(a) 
+print(b) 
+print(c) 
+print(d)
